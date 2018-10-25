deabd2bd2c4625f555d175bd2d02f19f1cb3c49f
Vaibahvi <kamani.v@husky.neu.edu>
2018-10-24 23:24:27 -0400

    app changes

--- webapp/cloudproject/src/test/java/com/cloudproject/controller/loginControllerTest.java
+++ webapp/cloudproject/src/test/java/com/cloudproject/controller/loginControllerTest.java
@@ -1,11 +1,6 @@
 package com.cloudproject.controller;
 
 import org.hamcrest.Matchers;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
